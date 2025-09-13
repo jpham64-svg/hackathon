9602532d2ecacfc1262bf4efadcc24e24541b3aa
jpham64-svg <jpham64@horizon.csueastbay.edu>
2025-09-13 13:09:55 -0700

    Add files via upload

---
+++ Untitled Project/code.js
@@ -0,0 +1,24 @@
+onEvent("cyberS", "click", function( ) {
+  setScreen("cyber_security");
+});
+
+onEvent("compSci", "click", function( ) {
+  setScreen("comp_science");
+});
+
+onEvent("phys", "click", function( ) {
+  setScreen("physics");
+});
+
+
+
+onEvent("back", "click", function( ) {
+  setScreen("main_menu");
+});
+onEvent("back2", "click", function( ) {
+  setScreen("main_menu");
+});
+onEvent("back3", "click", function( ) {
+  setScreen("main_menu");
+});
+
